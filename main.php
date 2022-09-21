ac9ea5e758fa08541afcc35ca4cf519e38067464
sari7happy <sari7happy@gmail.com>
2022-09-21 23:05:14 +0900

    ”php、HTML実装”

--- main.php
+++ main.php
@@ -1,9 +1,17 @@
-
-<?php  
+<html>
+<body>
+    <link rel="stylesheet" href="style.css">   
+<?php
 //open http://localhost:3000/main.php
- echo '<h1>Hello'.'PHP</h1>';
-
- echo '<p>世界の皆さん</p>';
+    echo '<h1>Welcome'.'barkery</h1>';
+    // echo '<strong></strong>';
+    echo '<p>こちらのパン屋さんは2022年9月10日にOPENしたばかりの話題のパン屋さんです!</p>';
+    echo '<h2>パン好き方集まれ!</h2>';
 // シングルころんでつなぐ
+    // echo '<img src="img/pan.jpg"</img>
+    echo '<img src="img/pan.jpg" width=100px</img>';
+    // echo '<img src="border-radius:50px>"'
 
-
+?>
+<footer>
+    &copy love barkery
